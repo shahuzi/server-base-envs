1ae7b6d22a2fe6a0c7af991ec905db0cc0be00d4
shahuzi <acmlxg@163.com>
2024-08-14 21:39:56 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-
+work_dir=$(dirname $(readlink -f $0))
 # install ohmyzsh
 cd ~
-sudo apt install zsh
+sudo apt install -y zsh
 sudo chsh -s /usr/bin/zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
@@ -11,10 +11,10 @@ git clone --depth=1 https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/pl
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-cp config_files/.zshrc ~
+cp ${work_dir}/config_files/.zshrc ~
 source ~/.zshrc
 
 # install tmux 
 sudo apt -get install tmux 
-cp config_files/.tmux.conf ~
+cp ${work_dir}/config_files/.tmux.conf ~
 
